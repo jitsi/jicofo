875eab4c893f34b2f5d63716cc323ccfebfabb0b
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2015-09-08 17:24:57 -0500

    Reuses the Jetty integration implemented by Videobridge (in order to reduce source code duplication between the two projects).

--- src/main/java/org/jitsi/jicofo/auth/AuthBundleActivator.java
+++ src/main/java/org/jitsi/jicofo/auth/AuthBundleActivator.java
@@ -22,12 +22,12 @@ import net.java.sip.communicator.util.Logger;
 
 import org.eclipse.jetty.ajp.*;
 import org.eclipse.jetty.server.*;
-import org.eclipse.jetty.server.nio.*;
 import org.jitsi.service.configuration.*;
 import org.jitsi.util.*;
+import org.jitsi.videobridge.rest.*;
 import org.osgi.framework.*;
 
-import java.lang.reflect.*;
+import java.util.*;
 
 /**
  * Implements <tt>BundleActivator</tt> for the OSGi bundle responsible for
@@ -37,10 +37,11 @@ import java.lang.reflect.*;
  * @author Pawel Domas
  */
 public class AuthBundleActivator
-    implements BundleActivator
+    extends AbstractJettyBundleActivator
 {
     /**
-     * Prefix for config properties in this class.
+     * The prefix of the names of {@code ConfigurationService} and/or
+     * {@code System} properties defined by {@code AuthBundleActivator}.
      */
     private static final String AUTH_PNAME = "org.jitsi.jicofo.auth";
 
@@ -67,61 +68,17 @@ public class AuthBundleActivator
         = AUTH_PNAME + ".DISABLE_AUTOLOGIN";
 
     /**
-     * The name of the <tt>System</tt> and <tt>ConfigurationService</tt>
-     * property which specifies the port on which the servlet handling external
-     * authentication works. The default value is <tt>8888</tt>.
-     */
-    private static final String JETTY_PORT_PNAME
-        = AUTH_PNAME + ".jetty.port";
-
-    /**
-     * The name of the <tt>System</tt> and <tt>ConfigurationService</tt>
-     * property which specifies the keystore password to be utilized by
-     * <tt>SslContextFactory</tt> when authentication servlet works on HTTPS.
-     */
-    private static final String JETTY_SSLCONTEXTFACTORY_KEYSTOREPASSWORD
-        = AUTH_PNAME + ".jetty.sslContextFactory.keyStorePassword";
-
-    /**
-     * The name of the <tt>System</tt> and <tt>ConfigurationService</tt>
-     * property which specifies the keystore path to be utilized by
-     * <tt>SslContextFactory</tt> when authentication servlet works on HTTPS.
-     */
-    private static final String JETTY_SSLCONTEXTFACTORY_KEYSTOREPATH
-        = AUTH_PNAME + ".jetty.sslContextFactory.keyStorePath";
-
-    /**
-     * The name of the <tt>System</tt> and <tt>ConfigurationService</tt>
-     * property which specifies whether client certificate authentication is to
-     * be required by <tt>SslContextFactory</tt> when authentication servlet
-     * works on HTTPS.
-     */
-    private static final String JETTY_SSLCONTEXTFACTORY_NEEDCLIENTAUTH
-        = AUTH_PNAME + ".jetty.sslContextFactory.needClientAuth";
-
-    /**
-     * The name of the <tt>System</tt> and/or <tt>ConfigurationService</tt>
-     * property which specifies the port used by authentication servlet with
-     * HTTPS. The default value is <tt>8443</tt>.
-     */
-    private static final String JETTY_TLS_PORT_PNAME
-        = AUTH_PNAME + ".jetty.tls.port";
-
-    /**
-     * The <tt>Logger</tt>.
+     * The {@code Logger} used by the {@code AuthBundleActivator} class and its
+     * instances to print debug information.
      */
     private static final Logger logger
         = Logger.getLogger(AuthBundleActivator.class);
 
     /**
-     * The Jetty <tt>Server</tt> used to run authentication servlet.
-     */
-    private Server server;
-
-    /**
      * Reference to service registration of {@link AuthenticationAuthority}.
      */
-    private ServiceRegistration<AuthenticationAuthority>
+    private
+        ServiceRegistration<AuthenticationAuthority>
             authAuthorityServiceRegistration;
 
     /**
@@ -130,6 +87,82 @@ public class AuthBundleActivator
     private AuthenticationAuthority authAuthority;
 
     static BundleContext bundleContext;
+
+    /**
+     * Initializes a new {@code AuthBundleActivator} instance.
+     */
+    public AuthBundleActivator()
+    {
+        super(AUTH_PNAME);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected int getDefaultPort()
+    {
+        // The idea of overriding Videobridge's default is probably an attempt
+        // to have Videobridge and Jicofo running on the same machine with their
+        // defaults and without them clashing.
+        return 8888;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected int getDefaultTlsPort()
+    {
+        // The idea of overriding Videobridge's default is probably an attempt
+        // to have Videobridge and Jicofo running on the same machine with their
+        // defaults and without them clashing.
+        return 8843;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Handler initializeHandlerList(
+            BundleContext bundleContext,
+            Server server)
+        throws Exception
+    {
+        List<Handler> handlers = new ArrayList<Handler>();
+
+        if (authAuthority instanceof ShibbolethAuthAuthority)
+        {
+            ShibbolethAuthAuthority shibbolethAuthAuthority
+                = (ShibbolethAuthAuthority) authAuthority;
+
+            handlers.add(new ShibbolethHandler(shibbolethAuthAuthority));
+        }
+
+        return initializeHandlerList(handlers);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Server initializeServer(BundleContext bundleContext)
+        throws Exception
+    {
+        Server server = super.initializeServer(bundleContext);
+
+        if (authAuthority instanceof ShibbolethAuthAuthority)
+        {
+            // AJP
+            Ajp13SocketConnector ajp13SocketConnector
+                = new Ajp13SocketConnector();
+
+            ajp13SocketConnector.setPort(8009);
+            server.addConnector(ajp13SocketConnector);
+        }
+
+        return server;
+    }
 
     /**
      * {@inheritDoc}
@@ -141,182 +174,39 @@ public class AuthBundleActivator
         AuthBundleActivator.bundleContext = bundleContext;
 
         ConfigurationService cfg
-                = ServiceUtils.getService(
-                        bundleContext,
-                        ConfigurationService.class);
+            = ServiceUtils.getService(
+                    bundleContext,
+                    ConfigurationService.class);
         String loginUrl = cfg.getString(LOGIN_URL_PNAME);
 
-        if (StringUtils.isNullOrEmpty(loginUrl))
+        if (!StringUtils.isNullOrEmpty(loginUrl))
         {
-            return;
-        }
+            logger.info("Starting authentication service... URL: " + loginUrl);
 
-        logger.info("Starting authentication service... URL: " + loginUrl);
-
-        if (loginUrl.toUpperCase().startsWith("XMPP:"))
-        {
-            this.authAuthority
-                = new XMPPDomainAuthAuthority(loginUrl.substring(5));
-        }
-        else
-        {
-            String logoutUrl = cfg.getString(LOGOUT_URL_PNAME);
-
-            this.authAuthority
-                = new ShibbolethAuthAuthority(loginUrl, logoutUrl);
-        }
-
-        logger.info("Auth authority: " + authAuthority);
-
-        authAuthorityServiceRegistration
-            = bundleContext.registerService(
-                    AuthenticationAuthority.class, authAuthority, null);
-
-        authAuthority.start();
-
-        // FIXME move Jetty related code to separate class
-        if (!(authAuthority instanceof ShibbolethAuthAuthority))
-        {
-            return;
-        }
-
-        ShibbolethAuthAuthority shibbolethAuthAuthority
-            = (ShibbolethAuthAuthority) authAuthority;
-
-        // The REST API of Videobridge does not start by default.
-        int port = 8888, tlsPort = 8843;
-        String sslContextFactoryKeyStorePassword, sslContextFactoryKeyStorePath;
-        boolean sslContextFactoryNeedClientAuth = false;
-
-        if (cfg == null)
-        {
-            port = Integer.getInteger(JETTY_PORT_PNAME, port);
-            sslContextFactoryKeyStorePassword
-                = System.getProperty(JETTY_SSLCONTEXTFACTORY_KEYSTOREPASSWORD);
-            sslContextFactoryKeyStorePath
-                = System.getProperty(JETTY_SSLCONTEXTFACTORY_KEYSTOREPATH);
-            sslContextFactoryNeedClientAuth
-                = Boolean.getBoolean(JETTY_SSLCONTEXTFACTORY_NEEDCLIENTAUTH);
-            tlsPort = Integer.getInteger(JETTY_TLS_PORT_PNAME, tlsPort);
-        }
-        else
-        {
-            port = cfg.getInt(JETTY_PORT_PNAME, port);
-            sslContextFactoryKeyStorePassword
-                = cfg.getString(JETTY_SSLCONTEXTFACTORY_KEYSTOREPASSWORD);
-            sslContextFactoryKeyStorePath
-                = cfg.getString(JETTY_SSLCONTEXTFACTORY_KEYSTOREPATH);
-            sslContextFactoryNeedClientAuth
-                = cfg.getBoolean(
-                        JETTY_SSLCONTEXTFACTORY_NEEDCLIENTAUTH,
-                        sslContextFactoryNeedClientAuth);
-            tlsPort = cfg.getInt(JETTY_TLS_PORT_PNAME, tlsPort);
-        }
-
-        try
-        {
-            Server server = new Server();
-
-            /*
-             * If HTTPS is not enabled, serve the REST API of Jitsi Videobridge
-             * over HTTP.
-             */
-            if (sslContextFactoryKeyStorePath == null)
+            if (loginUrl.toUpperCase().startsWith("XMPP:"))
             {
-                // HTTP
-                SelectChannelConnector httpConnector
-                    = new SelectChannelConnector();
-
-                httpConnector.setPort(port);
-                server.addConnector(httpConnector);
+                authAuthority
+                    = new XMPPDomainAuthAuthority(loginUrl.substring(5));
             }
             else
             {
-                // FIXME: add HTTPS
-                //HttpConfiguration httpCfg = new HttpConfiguration();
+                String logoutUrl = cfg.getString(LOGOUT_URL_PNAME);
 
-                //httpCfg.setSecurePort(tlsPort);
-                //httpCfg.setSecureScheme("https");
-                /*File sslContextFactoryKeyStoreFile
-                    = getAbsoluteFile(sslContextFactoryKeyStorePath, cfg);
-                SslContextFactory sslContextFactory = new SslContextFactory();
-
-                sslContextFactory.setExcludeCipherSuites(
-                        "SSL_RSA_WITH_DES_CBC_SHA",
-                        "SSL_DHE_RSA_WITH_DES_CBC_SHA",
-                        "SSL_DHE_DSS_WITH_DES_CBC_SHA",
-                        "SSL_RSA_EXPORT_WITH_RC4_40_MD5",
-                        "SSL_RSA_EXPORT_WITH_DES40_CBC_SHA",
-                        "SSL_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA",
-                        "SSL_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA");
-                sslContextFactory.setIncludeCipherSuites(".*RC4.*");
-                if (sslContextFactoryKeyStorePassword != null)
-                {
-                    sslContextFactory.setKeyStorePassword(
-                            sslContextFactoryKeyStorePassword);
-                }
-                sslContextFactory.setKeyStorePath(
-                        sslContextFactoryKeyStoreFile.getPath());
-                sslContextFactory.setNeedClientAuth(
-                        sslContextFactoryNeedClientAuth);
-
-                HttpConfiguration httpsCfg = new HttpConfiguration(httpCfg);
-
-                httpsCfg.addCustomizer(new SecureRequestCustomizer());
-
-                ServerConnector sslConnector
-                    = new ServerConnector(
-                            server,
-                            new SslConnectionFactory(
-                                    sslContextFactory,
-                                    "http/1.1"),
-                            new HttpConnectionFactory(httpsCfg));
-                sslConnector.setPort(tlsPort);
-                server.addConnector(sslConnector);*/
-
-                /*
-                The example borrowed from Jetty 8 docs
-                SslSelectChannelConnector ssl_connector = new SslSelectChannelConnector();
-                String jetty_home =
-                  System.getProperty("jetty.home","../jetty-distribution/target/distribution");
-                System.setProperty("jetty.home",jetty_home);
-                ssl_connector.setPort(8443);
-                SslContextFactory cf = ssl_connector.getSslContextFactory();
-                cf.setKeyStore(jetty_home + "/etc/keystore");
-                cf.setKeyStorePassword("OBF:1vn2343223434f43rt5sg23241zlu1vn4");
-                cf.setKeyManagerPassword("OBF:1234324fa4lf4351u2g");
-                 */
+                authAuthority
+                    = new ShibbolethAuthAuthority(loginUrl, logoutUrl);
             }
+            logger.info("Auth authority: " + authAuthority);
 
-            // AJP
-            Ajp13SocketConnector ajp13SocketConnector
-                = new Ajp13SocketConnector();
-            ajp13SocketConnector.setPort(8009);
+            authAuthorityServiceRegistration
+                = bundleContext.registerService(
+                        AuthenticationAuthority.class,
+                        authAuthority,
+                        null);
 
-            server.addConnector(ajp13SocketConnector);
-
-            server.setHandler(
-                    new ShibbolethHandler(shibbolethAuthAuthority));
-
-            /*
-             * The server will start a non-daemon background Thread which will
-             * keep the application running on success. 
-             */
-            server.start();
-
-            this.server = server;
+            authAuthority.start();
         }
-        catch (Throwable t)
-        {
-            // Log any Throwable for debugging purposes and rethrow.
-            logger.error("Failed to start external authentication bundle.", t);
-            if (t instanceof Error)
-                throw (Error) t;
-            else if (t instanceof Exception)
-                throw (Exception) t;
-            else
-                throw new UndeclaredThrowableException(t);
-        }
+
+        super.start(bundleContext);
     }
 
     /**
@@ -337,12 +227,26 @@ public class AuthBundleActivator
             authAuthority = null;
         }
 
-        if (server != null)
-        {
-            server.stop();
-            server = null;
-        }
+        super.stop(bundleContext);
 
         AuthBundleActivator.bundleContext = null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean willStart(BundleContext bundleContext)
+        throws Exception
+    {
+        boolean b = super.willStart(bundleContext);
+
+        if (b)
+        {
+            // Shibboleth works the same as every other web-based Single Sign-on
+            // (SSO) system so it requires the Jetty HTTP server.
+            b = (authAuthority instanceof ShibbolethAuthAuthority);
+        }
+        return b;
     }
 }
