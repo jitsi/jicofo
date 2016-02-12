1c8001e9d0bdcf2e6a6c59573860176a6d3b9f81
Martin Sebastian <martin.sebastian1@gmail.com>
2016-02-12 17:52:54 +0530

    Fix for issue "Focus must be Owner"

--- src/main/java/org/jitsi/impl/protocol/xmpp/ChatRoomImpl.java
+++ src/main/java/org/jitsi/impl/protocol/xmpp/ChatRoomImpl.java
@@ -169,6 +169,7 @@ public class ChatRoomImpl
     public void joinAs(String nickname)
         throws OperationFailedException
     {
+        boolean ownerNotAvailable = true;
         try
         {
             this.myNickName = nickname;
@@ -189,6 +190,14 @@ public class ChatRoomImpl
 
             muc.create(nickname);
             //muc.join(nickname);
+            
+            for (Occupant occupant : muc.getModerators())
+            {
+                if (occupant.getAffiliation().equalsIgnoreCase("owner"))
+                {
+                    ownerNotAvailable = false;
+                }
+            }
 
             // Make the room non-anonymous, so that others can
             // recognize focus JID
@@ -232,8 +241,13 @@ public class ChatRoomImpl
 
             muc.sendConfigurationForm(answer);
         }
-        catch (XMPPException e)
+        catch (XMPPException | ClassCastException e)
         {
+            if (ownerNotAvailable)
+            {
+                logger.warn("No moderator in room!! Looks like the room is disposed.");
+                return;
+            }
             throw new OperationFailedException(
                 "Failed to join the room",
                 OperationFailedException.GENERAL_ERROR, e);
