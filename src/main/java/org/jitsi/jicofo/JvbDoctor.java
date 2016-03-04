b8c3fb864e0cba5468378d4b927e2bd8caf2907a
paweldomas <pawel.domas@jitsi.org>
2016-03-04 11:34:27 -0600

    Will give the health-check another try on 1st timeout.

--- src/main/java/org/jitsi/jicofo/JvbDoctor.java
+++ src/main/java/org/jitsi/jicofo/JvbDoctor.java
@@ -66,6 +66,14 @@ public class JvbDoctor
         = "org.jitsi.jicofo.HEALTH_CHECK_INTERVAL";
 
     /**
+     * The name of the configuration property used to configure 2nd chance
+     * delay. This is how long we will wait to retry the health check after 1st
+     * timeout.
+     */
+    public static final String SECOND_CHANCE_DELAY_PNAME
+        = "org.jitsi.jicofo.HEALTH_CHECK_2NDTRY_DELAY";
+
+    /**
      * Default value for JVB health checks is 10 seconds.
      */
     public static final long DEFAULT_HEALTH_CHECK_INTERVAL = 10000;
@@ -82,7 +90,13 @@ public class JvbDoctor
     /**
      * Tells how often we send health checks to the bridge in ms.
      */
-    private long healthCheckInterval = DEFAULT_HEALTH_CHECK_INTERVAL;
+    private long healthCheckInterval;
+
+    /**
+     * 2nd chance delay which tells how long we will wait to retry the health
+     * check after 1st attempt has timed out.
+     */
+    private long secondChanceDelay;
 
     /**
      * OSGi bundle context.
@@ -151,6 +165,11 @@ public class JvbDoctor
             logger.warn("JVB health-checks disabled");
             return;
         }
+
+        secondChanceDelay
+            = FocusBundleActivator.getConfigService().getLong(
+                    SECOND_CHANCE_DELAY_PNAME,
+                    DEFAULT_HEALTH_CHECK_INTERVAL / 2);
 
         this.osgiBc = bundleContext;
 
@@ -338,14 +357,17 @@ public class JvbDoctor
 
         private boolean checkTaskStillValid()
         {
-            if (!tasks.containsKey(bridgeJid))
+            synchronized (JvbDoctor.this)
             {
-                logger.info(
-                        "Health check task cancelled for: " + bridgeJid
-                            + " - response processing skipped");
-                return false;
+                if (!tasks.containsKey(bridgeJid))
+                {
+                    logger.info(
+                            "Health check task cancelled for: " + bridgeJid
+                                + " - response processing skipped");
+                    return false;
+                }
+                return true;
             }
-            return true;
         }
 
         private void verifyHealthCheckSupport()
@@ -373,6 +395,14 @@ public class JvbDoctor
             }
         }
 
+        private HealthCheckIQ newHealthCheckIQ(String bridgeJid)
+        {
+            HealthCheckIQ healthIq = new HealthCheckIQ();
+            healthIq.setTo(bridgeJid);
+            healthIq.setType(IQ.Type.GET);
+            return healthIq;
+        }
+
         private void doHealthCheck()
         {
             // If XMPP is currently not connected skip the health-check
@@ -385,10 +415,6 @@ public class JvbDoctor
             }
 
             XmppConnection connection;
-
-            HealthCheckIQ healthIq = new HealthCheckIQ();
-            healthIq.setTo(bridgeJid);
-            healthIq.setType(IQ.Type.GET);
 
             // Sync on start/stop and bridges state
             synchronized (JvbDoctor.this)
@@ -410,7 +436,34 @@ public class JvbDoctor
                 logger.debug("Sending health-check request to: " + bridgeJid);
             }
 
-            Packet response = connection.sendPacketAndGetReply(healthIq);
+            Packet response = connection.sendPacketAndGetReply(
+                    newHealthCheckIQ(bridgeJid));
+
+            // On timeout we'll give it one more try
+            if (response == null && secondChanceDelay > 0)
+            {
+                try
+                {
+                    if (!checkTaskStillValid())
+                        return;
+
+                    logger.warn(bridgeJid + " health-check timed out,"
+                            + " but will give it another try after: "
+                            + secondChanceDelay);
+
+                    Thread.sleep(secondChanceDelay);
+
+                    if (!checkTaskStillValid())
+                        return;
+
+                    response = connection.sendPacketAndGetReply(
+                            newHealthCheckIQ(bridgeJid));
+                }
+                catch (InterruptedException e)
+                {
+                    logger.error("Second chance delay wait interrupted", e);
+                }
+            }
 
             // Sync on start/stop and bridges state
             synchronized (JvbDoctor.this)
