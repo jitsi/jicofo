2dd8a96419595ca7957ce7c08413beab1cd7718b
bbaldino <bbaldino@gmail.com>
2017-02-09 09:08:35 -0800

    Offer h264 (after vp8) so interested clients can use it (#165)

--- src/main/java/org/jitsi/jicofo/util/JingleOfferFactory.java
+++ src/main/java/org/jitsi/jicofo/util/JingleOfferFactory.java
@@ -221,6 +221,35 @@ public class JingleOfferFactory
                 vp8, "x-google-start-bitrate", String.valueOf(startBitrate));
         }
 
+        // a=rtpmap:107 H264/90000
+        int h264pt = 107;
+        PayloadTypePacketExtension h264
+            = addPayloadTypeExtension(rtpDesc, h264pt, Constants.H264, 90000);
+
+        // a=rtcp-fb:107 ccm fir
+        h264.addRtcpFeedbackType(createRtcpFbPacketExtension("ccm", "fir"));
+
+        // a=rtcp-fb:107 nack
+        h264.addRtcpFeedbackType(createRtcpFbPacketExtension("nack", null));
+
+        // a=rtcp-fb:107 nack pli
+        h264.addRtcpFeedbackType(createRtcpFbPacketExtension("nack", "pli"));
+
+        // a=rtcp-fb:107 goog-remb
+        h264.addRtcpFeedbackType(createRtcpFbPacketExtension("goog-remb", null));
+
+        if (minBitrate != -1)
+        {
+            addParameterExtension(
+                h264, "x-google-min-bitrate", String.valueOf(minBitrate));
+        }
+
+        if (startBitrate != -1)
+        {
+            addParameterExtension(
+                h264, "x-google-start-bitrate", String.valueOf(startBitrate));
+        }
+
         if (useRtx)
         {
             // a=rtpmap:96 rtx/90000
