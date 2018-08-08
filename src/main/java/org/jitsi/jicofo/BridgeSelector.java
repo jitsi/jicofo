bcdd01da00f91bcf7ae894443cc4e4902cb97274
Boris Grozev <boris@jitsi.org>
2018-08-08 12:43:28 -0500

    feat: Adds a region-based selection strategy.

--- src/main/java/org/jitsi/jicofo/BridgeSelector.java
+++ src/main/java/org/jitsi/jicofo/BridgeSelector.java
@@ -35,6 +35,7 @@ import org.jxmpp.stringprep.*;
 import org.osgi.framework.*;
 
 import java.util.*;
+import java.util.stream.*;
 
 /**
  * Class exposes methods for selecting best videobridge from all currently
@@ -881,6 +882,85 @@ public class BridgeSelector
             }
 
             return null;
+        }
+    }
+
+    /**
+     * Implements a {@link BridgeSelectionStrategy} which
+     */
+    private static class RegionBasedBridgeSelectionStrategy
+        extends BridgeSelectionStrategy
+    {
+        /**
+         * Default constructor.
+         */
+        RegionBasedBridgeSelectionStrategy()
+        {}
+
+        /**
+         * {@inheritDoc}
+         * </p>
+         * Always selects the bridge already used by the conference.
+         */
+        @Override
+        public Bridge doSelect(
+            List<Bridge> bridges,
+            List<Bridge> conferenceBridges,
+            JitsiMeetConference conference,
+            String participantRegion)
+        {
+            if (participantRegion == null)
+            {
+                // We don't know the participant's region. Use the least loaded
+                // existing bridge in the conference.
+                return selectFirst(bridges, conferenceBridges);
+            }
+
+            // We know the participant's region.
+            List<Bridge> conferenceBridgesInRegion
+                = conferenceBridges.stream()
+                    .filter(
+                        bridge -> participantRegion.equals(bridge.getRegion()))
+                    .collect(Collectors.toList());
+            if (!conferenceBridgesInRegion.isEmpty())
+            {
+                return selectFirst(bridges, conferenceBridgesInRegion);
+            }
+
+            // The conference has no bridges in the participant region. Try
+            // to add a new bridge in that region.
+            Bridge bridgeInRegion
+                = bridges.stream()
+                    .filter(bridge -> participantRegion.equals(bridge.getRegion()))
+                    .findFirst().orElse(null);
+            if (bridgeInRegion != null)
+            {
+                return bridgeInRegion;
+            }
+
+            // We couldn't find a bridge in the participant's region. Use the
+            // least loaded of the existing conference bridges.
+            // TODO: perhaps use a bridge in a nearby region (if we have data
+            // about the topology of the regions).
+            return selectFirst(bridges, conferenceBridges);
+        }
+
+        /**
+         * Selects the bridge from {@code selectFrom} which occurs first in
+         * {@code order}.
+         * @param order A list which dictates the order.
+         * @param selectFrom A list of bridges to select from. Assumed non-null
+         * and non-empty.
+         * @return the bridge from {@code selectFrom} which occurs first in
+         * {@code order}.
+         */
+        private Bridge selectFirst(
+            List<Bridge> selectFrom, List<Bridge> order)
+        {
+            return order.stream()
+                .filter(selectFrom::contains)
+                .findFirst()
+                .orElse(selectFrom.get(0));
         }
     }
 }
