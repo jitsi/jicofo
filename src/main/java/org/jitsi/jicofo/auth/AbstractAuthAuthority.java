f785254455b0e9de0fd8ff284f4a6bb096a2af0d
Justin Martinez <justin.martinez@readytalk.com>
2018-08-29 12:22:20 -0600

    fix deadlock

--- src/main/java/org/jitsi/jicofo/auth/AbstractAuthAuthority.java
+++ src/main/java/org/jitsi/jicofo/auth/AbstractAuthAuthority.java
@@ -410,10 +410,7 @@ public abstract class AbstractAuthAuthority
     public IQ processAuthentication(
             ConferenceIq query, ConferenceIq response)
     {
-        synchronized (syncRoot)
-        {
-            return processAuthLocked(query, response);
-        }
+        return processAuthLocked(query, response);
     }
 
     /**
