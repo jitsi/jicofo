659a3fc2c25afda16cffbbead96dc2e6fbc3a795
damencho <damencho@jitsi.org>
2018-08-29 18:57:36 -0500

    Removes duplicate constant.

--- src/main/java/org/jitsi/jicofo/Bridge.java
+++ src/main/java/org/jitsi/jicofo/Bridge.java
@@ -20,6 +20,7 @@ package org.jitsi.jicofo;
 import net.java.sip.communicator.impl.protocol.jabber.extensions.colibri.*;
 import org.jitsi.jicofo.discovery.*;
 import org.jitsi.jicofo.event.*;
+import org.jitsi.jicofo.jigasi.*;
 import org.jitsi.util.*;
 import org.jxmpp.jid.*;
 
@@ -49,16 +50,6 @@ class Bridge
      * to avoid depending on the {@code jitsi-videobridge} maven package.
      */
     private static final String STAT_NAME_VIDEO_STREAMS = "videostreams";
-
-    /**
-     * The name of the stat that indicates the bridge has entered graceful
-     * shutdown mode.
-     * {@code VideobridgeStatistics.SHUTDOWN_IN_PROGRESS}, but is defined
-     * separately to avoid depending on the {@code jitsi-videobridge} maven
-     * package.
-     */
-    public static final String STAT_NAME_SHUTDOWN_IN_PROGRESS
-        = "graceful_shutdown";
 
     /**
      * The name of the stat used by jitsi-videobridge to indicate its region.
@@ -163,8 +154,8 @@ class Bridge
             setVideoStreamCount(videoStreamCount);
         }
 
-        setIsOperational(!Boolean.valueOf(
-            stats.getValueAsString(STAT_NAME_SHUTDOWN_IN_PROGRESS)));
+        setIsOperational(!Boolean.valueOf(stats.getValueAsString(
+            JigasiDetector.STAT_NAME_SHUTDOWN_IN_PROGRESS)));
     }
 
     Bridge(BridgeSelector bridgeSelector,
