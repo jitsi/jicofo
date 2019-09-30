ff5e7954409a55e9846d409714bbdfa175ff6e9b
Yury Yaroshevich <yura.yaroshevich@gmail.com>
2019-09-30 13:50:50 +0300

    Avoid usage of Videobridge.getOrCreateLocalEndpoint method (#391)
    * Adapted endpoint retrieval to JVB's PR #927.
    
    * Use existing API to get endpoint.

--- src/test/java/mock/jvb/MockVideobridge.java
+++ src/test/java/mock/jvb/MockVideobridge.java
@@ -179,7 +179,7 @@ public class MockVideobridge
             String confId, String endpointId)
     {
         Conference conference = bridge.getConference(confId, null);
-        Endpoint endpoint = conference.getOrCreateLocalEndpoint(endpointId);
+        AbstractEndpoint endpoint = conference.getEndpoint(endpointId);
 
         MediaStreamTrackDesc[] tracks = endpoint.getMediaStreamTracks();
 
