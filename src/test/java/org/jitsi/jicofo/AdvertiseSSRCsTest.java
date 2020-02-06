jitsi/jicofo

commit b70696ec1d8e6cdb39bed3bc4627619202529b16

fix: validate conference state on source removal
When processing client's source remove notification, first tries to
remove on conference state copy and prevents the modification if it
results in a broken state.

src/main/java/org/jitsi/jicofo/JitsiMeetConferenceImpl.java
@@ -1865,10 +1865,8 @@ public class JitsiMeetConferenceImpl
         MediaSourceGroupMap sourceGroupsToRemove
             = MediaSourceGroupMap.getSourceGroupsForContents(contents);
 
-        removeSources(
-                sourceJingleSession, sourcesToRemove, sourceGroupsToRemove, true);
-
-        return null;
+        return removeSources(
+            sourceJingleSession, sourcesToRemove, sourceGroupsToRemove, true);
     }
 
     /**
@@ -1881,7 +1879,7 @@ public class JitsiMeetConferenceImpl
      * @param updateChannels tells whether or not sources update request should be
      *                       sent to the bridge.
      */
-    private void removeSources(JingleSession        sourceJingleSession,
+    private XMPPError removeSources(JingleSession        sourceJingleSession,
                                MediaSourceMap       sourcesToRemove,
                                MediaSourceGroupMap  sourceGroupsToRemove,
                                boolean              updateChannels)
@@ -1892,21 +1890,48 @@ public class JitsiMeetConferenceImpl
         if (participant == null)
         {
             logger.warn("Remove-source: no session for " + participantJid);
-            return;
+
+            return null;
+        }
+
+        final MediaSourceMap conferenceSources = getAllSources();
+        final MediaSourceGroupMap conferenceSourceGroups = getAllSourceGroups();
+
+        SSRCValidator validator
+                = new SSRCValidator(
+                        participant.getEndpointId(),
+                        conferenceSources,
+                        conferenceSourceGroups,
+                        globalConfig.getMaxSourcesPerUser(),
+                        this.logger);
+
+        Object[] removed;
+
+        try
+        {
+            removed
+                = validator.tryRemoveSourcesAndGroups(
+                        sourcesToRemove, sourceGroupsToRemove);
+        }
+        catch (InvalidSSRCsException e)
+        {
+            logger.error(
+                    "Error removing SSRCs from: " + participantJid
+                            + ": " + e.getMessage());
+            return XMPPError.from(
+                    XMPPError.Condition.bad_request, e.getMessage()).build();
         }
 
         // Only sources owned by this participant end up in "removed" set
-        final MediaSourceMap removedSources
-            = participant.removeSources(sourcesToRemove);
-
+        final MediaSourceMap removedSources = (MediaSourceMap) removed[0];
         final MediaSourceGroupMap removedGroups
-            = participant.removeSourceGroups(sourceGroupsToRemove);
+                = (MediaSourceGroupMap) removed[1];
 
         if (removedSources.isEmpty() && removedGroups.isEmpty())
         {
             logger.warn(
                     "No sources or groups to be removed from: "+ participantJid);
-            return;
+            return null;
         }
 
         // We remove all ssrc params from SourcePacketExtension as we want
@@ -1966,6 +1991,8 @@ public class JitsiMeetConferenceImpl
                             removedGroups);
                     }
                 });
+
+        return null;
     }
 
     /**

src/main/java/org/jitsi/jicofo/SSRCValidator.java
@@ -355,6 +355,35 @@ public class SSRCValidator
         return new Object[] { acceptedSources, acceptedGroups };
     }
 
+    /**
+     * Makes an attempt to remove given sources and source groups from
+     * the current state.
+     *
+     * @param sourcesToRemove the sources to be removed
+     * @param groupsToRemove the groups to be removed
+     *
+     * @return an array of two objects where first one is <tt>MediaSourceMap</tt>
+     * contains the sources that have been removed and the second one is
+     * <tt>MediaSourceGroupMap</tt> with <tt>SourceGroup</tt>s removed by this
+     * validator instance.
+     *
+     * @throws InvalidSSRCsException if a critical problem has been found
+     * after sources/groups removal which would probably would result in
+     * "setRemoteDescription" error on the client.
+     */
+    public Object[] tryRemoveSourcesAndGroups(
+            MediaSourceMap sourcesToRemove,
+            MediaSourceGroupMap groupsToRemove)
+        throws InvalidSSRCsException
+    {
+        MediaSourceMap removedSources = sources.remove(sourcesToRemove);
+        MediaSourceGroupMap removedGroups = sourceGroups.remove(groupsToRemove);
+
+        this.validateStreams();
+
+        return new Object[] { removedSources, removedGroups };
+    }
+
     private void filterOutParams(SourcePacketExtension copy)
     {
         Iterator<? extends ExtensionElement> params
@@ -436,8 +465,18 @@ public class SSRCValidator
                 continue;
             }
 
-            List<SimulcastGrouping> simGroupings
-                = sourceGroups.findSimulcastGroupings();
+            List<SimulcastGrouping> simGroupings;
+
+            try
+            {
+                simGroupings = sourceGroups.findSimulcastGroupings();
+            }
+            // If groups are in invalid state a SIM grouping may fail to
+            // initialize with IllegalArgumentException
+            catch (IllegalArgumentException exc)
+            {
+                throw new InvalidSSRCsException(exc.getMessage());
+            }
 
             // Check if this SIM group's MSID does not appear in any other
             // simulcast grouping

src/test/java/org/jitsi/jicofo/SSRCValidatorTest.java
@@ -715,6 +715,143 @@ public class SSRCValidatorTest
         assertEquals(1, groups.getSourceGroupsForMedia("video").size());
     }
 
+    @Test
+    public void testStateBrokenByRemoval()
+            throws InvalidSSRCsException
+    {
+        addDefaultVideoSSRCs();
+        addDefaultVideoGroups();
+
+        SSRCValidator ssrcValidator = createValidator();
+
+        ssrcValidator.tryAddSourcesAndGroups(sources, groups);
+
+        MediaSourceMap sourcesToRemove = new MediaSourceMap();
+        // remove SSRCs 10 and 20
+        sourcesToRemove.addSource("video", videoSources.get(0).copy());
+        sourcesToRemove.addSource("video", videoSources.get(1).copy());
+
+        MediaSourceGroupMap groupsToRemove = new MediaSourceGroupMap();
+        // remove SIM 10 30 50 group
+        groupsToRemove.addSourceGroup("video", videoGroups.get(0).copy());
+        // remove FIR 10 20 group
+        groupsToRemove.addSourceGroup("video", videoGroups.get(1).copy());
+
+        try {
+            ssrcValidator.tryRemoveSourcesAndGroups(sourcesToRemove, groupsToRemove);
+
+            fail("Did not detect broken state after SIM and FIR groups removal");
+        } catch (InvalidSSRCsException exception) {
+            String msg = exception.getMessage();
+            String msgPattern
+                = "MSID conflict across FID groups: vstream vtrack, "
+                    + "SourceGroup\\[FID, ssrc=50, ssrc=60, ]@\\w+ conflicts "
+                    + "with group SourceGroup\\[FID, ssrc=30, ssrc=40, ]@\\w+";
+
+            if(!msg.matches(msgPattern)) {
+                fail("Fail error msg validation: " + msg);
+            }
+        }
+    }
+
+    @Test
+    public void testStateBrokenBySourceRemoval()
+            throws InvalidSSRCsException
+    {
+        addDefaultVideoSSRCs();
+        addDefaultVideoGroups();
+
+        SSRCValidator ssrcValidator = createValidator();
+
+        ssrcValidator.tryAddSourcesAndGroups(sources, groups);
+
+        MediaSourceMap sourcesToRemove = new MediaSourceMap();
+        // remove SSRCs 10
+        sourcesToRemove.addSource("video", videoSources.get(0).copy());
+
+        try {
+            ssrcValidator.tryRemoveSourcesAndGroups(sourcesToRemove, new MediaSourceGroupMap());
+
+            fail("Did not detect broken state after source removal");
+        } catch (InvalidSSRCsException exception) {
+            String msg = exception.getMessage();
+            String msgPattern
+                = "Source ssrc=10 not found in 'video' for group: "
+                    + "SourceGroup\\[SIM, ssrc=10, ssrc=30, ssrc=50, ]@\\w+";
+
+            if(!msg.matches(msgPattern)) {
+                fail("Fail error msg validation: " + msg);
+            }
+        }
+    }
+
+    @Test
+    public void testStateBrokenByGroupRemoval()
+            throws InvalidSSRCsException
+    {
+        addDefaultVideoSSRCs();
+        addDefaultVideoGroups();
+
+        SSRCValidator ssrcValidator = createValidator();
+
+        ssrcValidator.tryAddSourcesAndGroups(sources, groups);
+
+        MediaSourceMap sourcesToRemove = new MediaSourceMap();
+        MediaSourceGroupMap groupsToRemove = new MediaSourceGroupMap();
+
+        // remove SIM 10 30 50 group
+        groupsToRemove.addSourceGroup("video", videoGroups.get(0).copy());
+
+        try {
+            ssrcValidator.tryRemoveSourcesAndGroups(sourcesToRemove, groupsToRemove);
+
+            fail("Did not detect broken state after SIM group removal");
+        } catch (InvalidSSRCsException exception) {
+            String msg = exception.getMessage();
+            String msgPattern
+                = "MSID conflict across FID groups: vstream vtrack, "
+                    + "SourceGroup\\[FID, ssrc=30, ssrc=40, ]@\\w+ "
+                    + "conflicts with group "
+                    + "SourceGroup\\[FID, ssrc=10, ssrc=20, ]@\\w+";
+
+            if(!msg.matches(msgPattern)) {
+                fail("Fail error msg validation: " + msg);
+            }
+        }
+    }
+
+    @Test
+    public void testStateBrokenByFirGroupRemoval()
+            throws InvalidSSRCsException
+    {
+        addDefaultVideoSSRCs();
+        addDefaultVideoGroups();
+
+        SSRCValidator ssrcValidator = createValidator();
+
+        ssrcValidator.tryAddSourcesAndGroups(sources, groups);
+
+        MediaSourceMap sourcesToRemove = new MediaSourceMap();
+        MediaSourceGroupMap groupsToRemove = new MediaSourceGroupMap();
+        // remove FIR 10 20 group
+        groupsToRemove.addSourceGroup("video", videoGroups.get(1).copy());
+
+        try {
+            ssrcValidator.tryRemoveSourcesAndGroups(sourcesToRemove, groupsToRemove);
+
+            fail("Did not detect broken state after FIR group removal");
+        } catch (InvalidSSRCsException exception) {
+            String msg = exception.getMessage();
+            String msgPattern
+                = "SIM group size != FID group count: SourceGroup"
+                    + "\\[SIM, ssrc=10, ssrc=30, ssrc=50, ]@\\w+ != 2";
+
+            if(!msg.matches(msgPattern)) {
+                fail("Fail error msg validation: " + msg);
+            }
+        }
+    }
+
     private void verifySSRC(
         String cname, String msid, SourcePacketExtension ssrc)
     {

commit 1f894ccd8a850c2520dd0f1dcc83de14a33829d1

fix: remove sources from participants broken by b70696ec
..and add unit test which include source removal validation.

Adjust the MediaSourceMap to not accept duplicates. It is used by
MockParticipant to add sources and it should mimic current Jitsi Meet
client's behavior of not adding duplicated sources.

Jicofo is not 100% accurate with tracking unadvertised sources.
Some sources that were included in session-initiate are re-advertised
when session-accept is received from the client, because
they were scheduled for addition while the jingle session is not active
yet. A check on the client is easier(and it's been there forever) rather
than trying to achieve 100% accuracy in the current implementation.

src/main/java/org/jitsi/jicofo/JitsiMeetConferenceImpl.java
@@ -1935,6 +1935,9 @@ public class JitsiMeetConferenceImpl
             return null;
         }
 
+        participant.removeSources(removedSources);
+        participant.removeSourceGroups(removedGroups);
+
         // We remove all ssrc params from SourcePacketExtension as we want
         // the client to simply remove all lines corresponding to given SSRC and
         // not care about parameter's values we send.

src/main/java/org/jitsi/protocol/xmpp/util/MediaSourceMap.java
@@ -113,7 +113,7 @@ public class MediaSourceMap
     }
 
     /**
-     * Adds sources to this map. NOTE that duplicates will be stored in the map.
+     * Adds sources to this map. NOTE that duplicates will NOT be stored in the map.
      *
      * @param mediaType the media type of sources to be added to this map.
      *
@@ -122,9 +122,12 @@ public class MediaSourceMap
     public void addSources(
         String mediaType, Collection<SourcePacketExtension> newSources)
     {
-        // BEWARE! addAll will not detect duplications
-        // as .equals is not overridden
-        getSourcesForMedia(mediaType).addAll(newSources);
+        // Remove duplicates before adding
+        List<SourcePacketExtension> mediaSources = getSourcesForMedia(mediaType);
+
+        mediaSources.removeIf(s -> newSources.stream().anyMatch(s2 -> s.getSSRC() == s2.getSSRC()));
+
+        mediaSources.addAll(newSources);
     }
 
     /**

src/test/java/mock/MockParticipant.java
@@ -483,7 +483,7 @@ public class MockParticipant
 
                 remoteSSRCgroups.add(ssrcGroupMap);
 
-                logger.info("session-initiate received " + nick + " " + ssrcMap  + " groups: " + ssrcGroupMap);
+                logger.info(nick + " received session-initiate: " + ssrcMap  + " groups: " + ssrcGroupMap);
 
                 sourceLock.notifyAll();
             }
@@ -505,7 +505,7 @@ public class MockParticipant
 
                 remoteSSRCgroups.add(ssrcGroupMap);
 
-                logger.info("source-add received " + nick + " " + ssrcMap);
+                logger.info(nick + " received source-add " + ssrcMap);
 
                 try
                 {
@@ -536,8 +536,7 @@ public class MockParticipant
 
                 remoteSSRCgroups.remove(ssrcGroupsToRemove);
 
-                logger.info(
-                    "source-remove received " + nick + " " + ssrcsToRemove);
+                logger.info(nick + " source-remove received " + ssrcsToRemove);
 
                 try
                 {
@@ -598,6 +597,32 @@ public class MockParticipant
     public List<SourcePacketExtension> audioSourceAdd(int count)
     {
         return sourceAdd("audio", count, false, null);
+    }
+
+    public void audioSourceRemove(int count)
+    {
+        List<SourcePacketExtension> audioSources
+                = this.localSSRCs.getSourcesForMedia("audio");
+
+        if (audioSources.size() < count)
+        {
+            throw new IllegalArgumentException(
+                    "audio source size(" + audioSources.size()
+                            + ") < count(" + count + ")");
+        }
+
+        List<SourcePacketExtension> toRemove = new ArrayList<>(count);
+
+        for(int i = 0; i < count; i++)
+        {
+            toRemove.add(audioSources.remove(0));
+        }
+
+        MediaSourceMap removeMap = new MediaSourceMap();
+
+        removeMap.addSources("audio", toRemove);
+
+        jingle.sendRemoveSourceIQ(removeMap, null, jingleSession);
     }
 
     private List<SourcePacketExtension> sourceAdd(

src/test/java/org/jitsi/jicofo/AdvertiseSSRCsTest.java
@@ -152,6 +152,67 @@ public class AdvertiseSSRCsTest
     }
 
     @Test
+    public void testSourceRemoval()
+            throws Exception
+    {
+        EntityBareJid roomName = JidCreate.entityBareFrom(
+                "testSSRCremoval@conference.pawel.jitsi.net");
+        String serverName = "test-server";
+
+        TestConference testConf
+                = TestConference.allocate(osgi.bc, serverName, roomName);
+
+        MockProtocolProvider pps
+                = testConf.getFocusProtocolProvider();
+
+        MockMultiUserChatOpSet mucOpSet = pps.getMockChatOpSet();
+
+        MockMultiUserChat chat
+                = (MockMultiUserChat) mucOpSet.findRoom(roomName.toString());
+
+        // Join with all users
+        MockParticipant user1 = new MockParticipant("User1");
+        user1.setSsrcVideoType(SSRCInfoPacketExtension.CAMERA_VIDEO_TYPE);
+        user1.join(chat);
+
+        MockParticipant user2 = new MockParticipant("User2");
+        user2.setSsrcVideoType(SSRCInfoPacketExtension.SCREEN_VIDEO_TYPE);
+        user2.join(chat);
+
+        // Accept invite with all users
+        assertNotNull(user1.acceptInvite(4000));
+        assertNotNull(user2.acceptInvite(4000));
+
+        user1.waitForAddSource(2000);
+        user2.waitForAddSource(2000);
+
+        user2.audioSourceRemove(1);
+
+        assertNotNull(user1.waitForRemoveSource(500));
+
+        assertEquals(1, user1.getRemoteSSRCs("audio").size());
+        assertEquals(0, user1.getRemoteSSRCGroups("audio").size());
+
+        MockParticipant user3 = new MockParticipant("User3");
+        user3.join(chat);
+        assertNotNull(user3.acceptInvite(4000));
+
+        user1.waitForAddSource(2000);
+
+        assertEquals(2, user1.getRemoteSSRCs("audio").size());
+        assertEquals(2, user3.getRemoteSSRCs("audio").size());
+        // No groups
+        assertEquals(0, user1.getRemoteSSRCGroups("audio").size());
+        assertEquals(0, user3.getRemoteSSRCGroups("audio").size());
+
+        user3.leave();
+        user2.leave();
+        user1.leave();
+
+        testConf.stop();
+    }
+
+    @Test
     public void testDuplicatedSSRCs()
         throws Exception
     {