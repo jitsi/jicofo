aea4d2ba253b404caac96f4eee3c7cb8eee030a8
bbaldino <bbaldino@gmail.com>
2020-08-26 15:29:31 -0700

    adapt to jicoco changes (#582)
    * adapt to jicoco changes
    
    * update jicoco dep
    
    * update jvb dep

--- src/main/java/org/jitsi/jicofo/util/OsgiServiceBinder.java
+++ src/main/java/org/jitsi/jicofo/util/OsgiServiceBinder.java
@@ -3,6 +3,7 @@ package org.jitsi.jicofo.util;
 import org.glassfish.hk2.utilities.binding.*;
 import org.jitsi.health.*;
 import org.jitsi.osgi.*;
+import org.jitsi.version.*;
 import org.osgi.framework.*;
 
 public class OsgiServiceBinder extends AbstractBinder
@@ -19,7 +20,7 @@ public class OsgiServiceBinder extends AbstractBinder
     {
         bind(new FocusManagerProvider(bundleContext)).to(FocusManagerProvider.class);
         bind(new JibriStatsProvider(bundleContext)).to(JibriStatsProvider.class);
-        bind(new VersionServiceProvider(bundleContext)).to(VersionServiceProvider.class);
-        bind(new HealthCheckServiceProvider(bundleContext)).to(HealthCheckServiceProvider.class);
+        bind(new OsgiVersionServiceSupplier(bundleContext)).to(VersionServiceSupplier.class);
+        bind(new OsgiHealthCheckServiceSupplier(bundleContext)).to(HealthCheckServiceSupplier.class);
     }
 }
