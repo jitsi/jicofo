2a49d83fe6a4a725da006c3715fb66c34c09288a
gluckzhang <zhanglong3030@qq.com>
2020-10-20 10:49:19 +0200

    a test that checks the default value of minParticipants

---
+++ src/test/java/org/jitsi/jicofo/TestJitsiMeetConfig.java
@@ -0,0 +1,15 @@
+package org.jitsi.jicofo;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Map;
+import java.util.HashMap;
+
+public class TestJitsiMeetConfig {
+    @Test
+    public void testGetMinParticipants() {
+        Map<String, String> properties = new HashMap<String, String> ();
+        JitsiMeetConfig jitsiMeetConfig = new JitsiMeetConfig(properties);
+        Assert.assertEquals(2, jitsiMeetConfig.getMinParticipants());
+    }
+}
