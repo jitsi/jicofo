6bf88e90eb9b3d640ba7eaa5f6f2254c17cf35c6
Boris Grozev <boris@jitsi.org>
2021-01-19 15:59:29 -0600

    chore: Update license headers.

--- src/main/java/org/jitsi/impl/protocol/xmpp/ChatRoom.java
+++ src/main/java/org/jitsi/impl/protocol/xmpp/ChatRoom.java
@@ -1,7 +1,7 @@
 /*
  * Jitsi, the OpenSource Java VoIP and Instant Messaging client.
  *
- * Copyright @ 2015 Atlassian Pty Ltd
+ * Copyright @ 2015-Present 8x8, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/main/java/org/jitsi/jicofo/recording/jibri/OperationSetJibri.java
+++ src/main/java/org/jitsi/jicofo/recording/jibri/OperationSetJibri.java
@@ -1,7 +1,7 @@
 /*
  * Jicofo, the Jitsi Conference Focus.
  *
- * Copyright @ 2017 Atlassian Pty Ltd
+ * Copyright @ 2017-Present 8x8, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
