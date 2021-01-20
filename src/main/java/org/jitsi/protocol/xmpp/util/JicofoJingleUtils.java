32500e04305f7304425f773b5f2f5ccc25c2d79f
Boris Grozev <boris@jitsi.org>
2021-01-20 10:23:31 -0600

    ref: Remove ColibriConferenceIQUtil (unused).

--- src/main/java/org/jitsi/protocol/xmpp/util/JicofoJingleUtils.java
+++ src/main/java/org/jitsi/protocol/xmpp/util/JicofoJingleUtils.java
@@ -35,8 +35,7 @@ public class JicofoJingleUtils
      */
     public static void addBundleExtensions(JingleIQ jingleIQ)
     {
-        GroupPacketExtension group
-            = GroupPacketExtension.createBundleGroup(jingleIQ.getContentList());
+        GroupPacketExtension group = GroupPacketExtension.createBundleGroup(jingleIQ.getContentList());
 
         jingleIQ.addExtension(group);
     }
@@ -47,11 +46,9 @@ public class JicofoJingleUtils
      * @param audioMute the value to set for the {@code audio} attribute.
      * @param videoMute the value to set for the {@code video} attribute.
      */
-    public static void addStartMutedExtension(
-        JingleIQ jingleIQ, boolean audioMute, boolean videoMute)
+    public static void addStartMutedExtension(JingleIQ jingleIQ, boolean audioMute, boolean videoMute)
     {
-        StartMutedPacketExtension startMutedExt
-            = new StartMutedPacketExtension();
+        StartMutedPacketExtension startMutedExt = new StartMutedPacketExtension();
         startMutedExt.setAudioMute(audioMute);
         startMutedExt.setVideoMute(videoMute);
         jingleIQ.addExtension(startMutedExt);
