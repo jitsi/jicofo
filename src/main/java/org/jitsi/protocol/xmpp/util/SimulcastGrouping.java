3936f654fc90d0aac9c109efc50d692ab3f457d6
bgrozev <boris@jitsi.org>
2021-01-19 18:02:03 -0600

    chore: Update license headers. (#675)

--- src/main/java/org/jitsi/protocol/xmpp/util/SimulcastGrouping.java
+++ src/main/java/org/jitsi/protocol/xmpp/util/SimulcastGrouping.java
@@ -1,7 +1,7 @@
 /*
  * Jicofo, the Jitsi Conference Focus.
  *
- * Copyright @ Atlassian Pty Ltd
+ * Copyright @ 2019-Present 8x8, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
