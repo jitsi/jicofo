eca73f948f6b24c4889e53f9266876c5dc6f0f9c
Boris Grozev <boris@jitsi.org>
2021-04-13 10:29:20 -0500

    fix: Run jibri IQ handling in the IO pool.

--- src/main/java/org/jitsi/jicofo/jibri/BaseJibri.java
+++ src/main/java/org/jitsi/jicofo/jibri/BaseJibri.java
@@ -118,7 +118,7 @@ public abstract class BaseJibri
                     }
                     return true;
                 },
-                TaskPools.getScheduledPool()
+                TaskPools.getIoPool()
         );
 
     }
