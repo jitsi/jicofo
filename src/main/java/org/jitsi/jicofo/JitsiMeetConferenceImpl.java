2b02555b073422704ba09cde3f321dfd8b9b5525
Boris Grozev <boris@jitsi.org>
2021-05-11 12:02:34 -0500

    log: Add the meetingId to the log context.

--- src/main/java/org/jitsi/jicofo/JitsiMeetConferenceImpl.java
+++ src/main/java/org/jitsi/jicofo/JitsiMeetConferenceImpl.java
@@ -464,6 +464,10 @@ public class JitsiMeetConferenceImpl
         transcriberManager.init();
 
         chatRoom.join();
+        if (chatRoom.getMeetingId() != null)
+        {
+            logger.addContext("meeting_id", chatRoom.getMeetingId());
+        }
 
         Collection<ExtensionElement> presenceExtensions = new ArrayList<>();
 
