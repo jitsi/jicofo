313934607e25d4d636e482d71a8f80a6371cc7ed
Boris Grozev <boris@jitsi.org>
2021-09-16 16:35:07 -0500

    fix: Signal REMB when TCC is not supported.

--- src/main/java/org/jitsi/jicofo/ParticipantChannelAllocator.java
+++ src/main/java/org/jitsi/jicofo/ParticipantChannelAllocator.java
@@ -106,6 +106,11 @@ public class ParticipantChannelAllocator extends AbstractChannelAllocator
         OfferOptions offerOptions = new OfferOptions();
         OfferOptionsKt.applyConstraints(offerOptions, config);
         OfferOptionsKt.applyConstraints(offerOptions, participant);
+        // Enable REMB only when TCC is not enabled.
+        if (!offerOptions.getTcc() && participant.hasRembSupport())
+        {
+            offerOptions.setRemb(true);
+        }
 
         return new Offer(new ConferenceSourceMap(), JingleOfferFactory.INSTANCE.createOffer(offerOptions));
     }
