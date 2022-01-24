8b2e7ca715b949207f49478cf01402de46cabcae
Boris Grozev <boris@jitsi.org>
2022-01-24 15:27:59 -0600

    squash: Fix docs.

--- src/main/java/org/jitsi/jicofo/conference/JitsiMeetConferenceImpl.java
+++ src/main/java/org/jitsi/jicofo/conference/JitsiMeetConferenceImpl.java
@@ -1395,8 +1395,8 @@ public class JitsiMeetConferenceImpl
      * @param participant the participant that owns the sources to be removed.
      * @param sourcesRequestedToBeRemoved the sources that an endpoint requested to be removed from the conference.
      * @param removeColibriSourcesFromLocalBridge whether to signal the source removal to the local bridge (we use
-     * @param sendSourceRemove Whether to send source-remove IQs to the remaining participants.
      * "false" to avoid sending an unnecessary "remove source" message just prior to the "expire" message).
+     * @param sendSourceRemove Whether to send source-remove IQs to the remaining participants.
      */
     private StanzaError removeSources(
             @NotNull Participant participant,
