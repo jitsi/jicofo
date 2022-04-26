197fe52e75d385b5a172962eb0925e320a33ea9d
Boris Grozev <boris@jitsi.org>
2022-04-26 11:38:19 -0500

    ref: Remove unused exception type.

--- src/main/kotlin/org/jitsi/jicofo/conference/colibri/ColibriAllocationFailedException.kt
+++ src/main/kotlin/org/jitsi/jicofo/conference/colibri/ColibriAllocationFailedException.kt
@@ -7,8 +7,7 @@ sealed class ColibriAllocationFailedException(message: String = "") : Exception(
 /** Bridge selection failed, i.e. there were no bridges available. */
 class BridgeSelectionFailedException : ColibriAllocationFailedException()
 class ColibriTimeoutException(val bridge: Bridge) : ColibriAllocationFailedException()
-class BridgeInGracefulShutdownException() : ColibriAllocationFailedException()
-class GenericColibriAllocationFailedException(message: String) : ColibriAllocationFailedException(message)
+class BridgeInGracefulShutdownException : ColibriAllocationFailedException()
 
 class ColibriConferenceDisposedException : ColibriAllocationFailedException()
 
