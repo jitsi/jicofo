85841eb5975dab5a6103104c0516db858cefe2d5
Boris Grozev <boris@jitsi.org>
2022-11-18 14:34:24 -0600

    ref: Move ParticipantTest to RateLimitTest.

--- jicofo/src/test/kotlin/org/jitsi/jicofo/util/RateLimitTest.kt
+++ jicofo/src/test/kotlin/org/jitsi/jicofo/util/RateLimitTest.kt
@@ -15,71 +15,63 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jitsi.jicofo
+package org.jitsi.jicofo.util
 
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
-import io.mockk.mockk
-import mock.muc.MockRoomMember
-import org.jitsi.jicofo.conference.Participant
 import org.jitsi.utils.secs
 import org.jitsi.utils.time.FakeClock
-import org.jxmpp.jid.impl.JidCreate
 
-class ParticipantTest : ShouldSpec() {
+class RateLimitTest : ShouldSpec() {
     init {
-        context("testRestartReqRateLimiting") {
+        context("RateLimit test") {
             val clock = FakeClock()
-            val p = Participant(
-                MockRoomMember(JidCreate.entityFullFrom("something@server.com/1234"), null),
-                conference = mockk(),
-                clock = clock
-            )
+            val rateLimit = RateLimit(clock = clock)
 
             should("allow 1st request") {
-                p.acceptRestartRequest() shouldBe true
+                rateLimit.accept() shouldBe true
             }
             should("not allow next request immediately") {
-                p.acceptRestartRequest() shouldBe false
+                rateLimit.accept() shouldBe false
             }
             clock.elapse(5.secs)
             should("not allow next request after 5 seconds") {
-                p.acceptRestartRequest() shouldBe false
+                rateLimit.accept() shouldBe false
             }
             clock.elapse(6.secs)
             should("allow 2nd request after 11 seconds") {
-                p.acceptRestartRequest() shouldBe true
+                rateLimit.accept() shouldBe true
             }
             should("not allow 3rd request after 11 seconds") {
-                p.acceptRestartRequest() shouldBe false
+                rateLimit.accept() shouldBe false
             }
             clock.elapse(10.secs)
             should("allow 3rd request after 21 seconds") {
-                p.acceptRestartRequest() shouldBe true
+                rateLimit.accept() shouldBe true
             }
             clock.elapse(11.secs)
             should("not allow more than 3 request within the last minute (31 second)") {
-                p.acceptRestartRequest() shouldBe false
+                rateLimit.accept() shouldBe false
             }
             clock.elapse(10.secs)
             should("not allow more than 3 request within the last minute (41 second)") {
-                p.acceptRestartRequest() shouldBe false
+                rateLimit.accept() shouldBe false
             }
             clock.elapse(10.secs)
             should("not allow more than 3 request within the last minute (51 second)") {
-                p.acceptRestartRequest() shouldBe false
+                rateLimit.accept() shouldBe false
             }
             clock.elapse(10.secs)
             should("allow the 4th request after 60 seconds have passed since the 1st (61 second)") {
-                p.acceptRestartRequest() shouldBe true
+                rateLimit.accept() shouldBe true
             }
             clock.elapse(5.secs)
             should("not allow the 5th request in 66th second") {
-                p.acceptRestartRequest() shouldBe false
+                rateLimit.accept() shouldBe false
             }
             clock.elapse(5.secs)
             should("allow the 5th request in 71st second") {
-                p.acceptRestartRequest() shouldBe true
+                rateLimit.accept() shouldBe true
             }
         }
     }
