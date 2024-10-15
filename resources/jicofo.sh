23c41df661a60570e4a459e82828d28aa104e706
damencho <damencho@jitsi.org>
2024-10-15 10:37:52 -0500

    squash: Adds a default value.

--- resources/jicofo.sh
+++ resources/jicofo.sh
@@ -9,11 +9,15 @@ if [[ "$1" == "--help"  ]]; then
 fi
 
 if [[ ! "$JAVA_SYS_PROPS" == *"-Dconfig.file="* ]]; then
-    echo
-    echo "To run jicofo you need a configuration file. Use environment variable JAVA_SYS_PROPS."
-    echo "e.g. export JAVA_SYS_PROPS=\"-Dconfig.file=/etc/jitsi/jicofo/jicofo.conf\""
-    echo
-    exit 2
+    if [[ -f /etc/jitsi/jicofo/jicofo.conf ]]; then
+        JAVA_SYS_PROPS="$JAVA_SYS_PROPS -Dconfig.file=/etc/jitsi/jicofo/jicofo.conf"
+    else
+        echo
+        echo "To run jicofo you need a configuration file. Use environment variable JAVA_SYS_PROPS."
+        echo "e.g. export JAVA_SYS_PROPS=\"-Dconfig.file=/etc/jitsi/jicofo/jicofo.conf\""
+        echo
+        exit 2
+    fi
 fi
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
