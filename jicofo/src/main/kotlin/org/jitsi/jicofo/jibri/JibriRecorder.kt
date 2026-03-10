e2a31062ade7291a75f8611ef605f13fc6f1180f
Boris Grozev <boris@jitsi.org>
2026-03-10 15:57:16 -0500

    squash: Remove unused import.

--- jicofo/src/main/kotlin/org/jitsi/jicofo/jibri/JibriRecorder.kt
+++ jicofo/src/main/kotlin/org/jitsi/jicofo/jibri/JibriRecorder.kt
@@ -18,7 +18,6 @@
 package org.jitsi.jicofo.jibri
 
 import org.apache.commons.lang3.StringUtils
-import org.jitsi.jicofo.TaskPools
 import org.jitsi.jicofo.conference.JitsiMeetConferenceImpl
 import org.jitsi.jicofo.jibri.JibriConfig.Companion.config
 import org.jitsi.jicofo.jibri.JibriSession.StartException
